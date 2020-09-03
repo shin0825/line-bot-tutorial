a83467cde8da549a1811a14041c1bba1c73201bc
shin0825 <43142294+shin0825@users.noreply.github.com>
2020-09-03 23:51:06 +0900

    とりあえずチュートリアル通りにコーディング

---
+++ line-bot-tutorial/app/controllers/linebot_controller.rb
@@ -0,0 +1,66 @@
+class LinebotController < ApplicationController
+  require 'line/bot'
+
+  protect_from_forgery :except => [:callback]
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      head :bad_request
+    end
+
+    events = client.parse_events_from(body)
+
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
+          if event.message['text'].eql?('アンケート')
+            # private内のtemplateメソッドを呼び出します。
+            client.reply_message(event['replyToken'], template)
+          end
+        end
+      end
+    }
+
+    head :ok
+  end
+
+  private
+
+  def template
+    {
+      "type": "template",
+      "altText": "非対応のデバイス。",
+      "template": {
+          "type": "confirm",
+          "text": "確認させてください。",
+          "actions": [
+              {
+                "type": "message",
+                # Botから送られてきたメッセージに表示される文字列です。
+                "label": "いいよ。",
+                # ボタンを押した時にBotに送られる文字列です。
+                "text": "いいよ。"
+              },
+              {
+                "type": "message",
+                "label": "ダメだね。",
+                "text": "ダメよ。"
+              }
+          ]
+      }
+    }
+  end
+end
